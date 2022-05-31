769c9b9766b141a3746ce4f7124dc73d50f2282c
omaranBazna <omran4d@gmail.com>
2022-05-31 08:43:35 -0400

    add the form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Title from './Components/Title'
 function App() {
   return (
     <div className="App">
-      
+      <Title />
     </div>
   );
 }
